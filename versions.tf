8abb77bfbcb86acf5ecbe060579f1a46184f0adc
Kumar <uday.h.kumar@capgemini.com>
2025-10-27 14:14:37 +0530

    tfe workspace test

--- versions.tf
+++ versions.tf
@@ -1,14 +1,6 @@
 terraform {
   required_version = ">= 1.3.0"
 
-  backend "remote" {
-    organization = "uday-devops-terraform"
-
-    workspaces {
-      name = "hashicorp-lab"
-    }
-  }
-
   required_providers {
     aws = {
       source  = "hashicorp/aws"
